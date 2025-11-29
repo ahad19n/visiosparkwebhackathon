835955634c2b859087c48cf558b4d225eed2ac2d
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-11-29 13:21:16 +0500

    swap gmail for nodemailer

--- server/utils/sendOTP.js
+++ server/utils/sendOTP.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const sendOTP = async (email, otp) => {
   const transporter = nodemailer.createTransport({
-    service: "Gmail", // or any other email provider
+    service: "Gmail", // or nodemailer
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
